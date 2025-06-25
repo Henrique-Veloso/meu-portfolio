378e203ca262bf5a52062f1affebe8237364d533
Henrique Veloso <henriquevelosos@hotmail.com>
2025-06-25 20:53:44 -0300

    Cores

--- portfolio/templatetags/portfolio_filters.py
+++ portfolio/templatetags/portfolio_filters.py
@@ -2,9 +2,16 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='multiply_by_200') 
+@register.filter(name='multiply_by_200')
 def multiply_by_200(value):
     try:
         return value * 200
     except (ValueError, TypeError):
+        return value
+
+@register.filter(name='split_string') 
+def split_string(value, delimiter):
+    
+    if not isinstance(value, str):
         return value 
+    return value.split(delimiter)
